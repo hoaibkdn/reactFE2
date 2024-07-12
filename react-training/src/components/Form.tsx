513db38da6aca325b05496b4301b68357a01a2d4
hoaitruong <hoaibkdn2012@gmail.com>
2024-07-12 09:05:14 -0700

    separate form

--- react-training/src/components/Form.tsx
+++ react-training/src/components/Form.tsx
@@ -1,5 +1,6 @@
 /** @format */
-import { memo, ChangeEvent, useState, useRef } from 'react';
+import { memo, ChangeEvent, useState, useRef, useCallback } from 'react';
+import Input from './Input';
 
 const Form = ({
   firstname,
@@ -9,13 +10,22 @@ const Form = ({
   lastname: string;
 }) => {
   console.log('form');
-
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [form, setForm] = useState({
+    title: '',
+    content: '',
+    author: '',
+  });
   const titleRef = useRef<HTMLInputElement>(null);
-  const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value);
-  };
+
+  const onChangeForm = useCallback(
+    (e: ChangeEvent<HTMLInputElement>, inputType: string) => {
+      setForm({
+        ...form,
+        [inputType]: e.target.value,
+      });
+    },
+    [form]
+  );
   const onSubmit = (e: any) => {
     e.preventDefault(); //
 
@@ -27,22 +37,19 @@ const Form = ({
   return (
     <form onSubmit={onSubmit}>
       <h3>Form {firstname}</h3>
-
-      <div>
-        <label>Title</label>
-        <input
-          ref={titleRef}
-          type='text'
-          value={title} // controlled component
-          // defaultValue={'Hello world'}
-          onChange={onChangeTitle}
-        />
-      </div>
-
-      <div>
-        <label>Content</label>
-        <input type='text' value={content} />
-      </div>
+      <Input
+        label='Title'
+        value={form.title}
+        onChange={onChangeForm}
+        inputType='title'
+      />
+      <Input
+        label='Content'
+        value={form.content}
+        onChange={onChangeForm}
+        inputType='content'
+      />
+      {/* Input author */}
       <button type='submit'>Submit</button>
     </form>
   );
